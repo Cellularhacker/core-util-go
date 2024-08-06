6499529fc8655ff9b1f1e099fc33b644065d50a2
Cellularhacker <wva3cdae@gmail.com>
2024-08-06 13:24:01 +0900

    Added localization for timezone

---
+++ localization.go
@@ -0,0 +1,17 @@
+package core
+
+import "time"
+
+//
+// MARK: Full name funcs
+
+func GetLocation() *time.Location {
+	return loc
+}
+
+//
+// MARK: Shorten name funcs
+
+func GetLoc() *time.Location {
+	return loc
+}
