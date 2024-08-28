604d381b3c07f03606255528ebde6d8eb0c34b9e
Cellularhacker <wva3cdae@gmail.com>
2024-08-28 10:10:28 +0900

    Added timezone

--- timezone.go
+++ timezone.go
@@ -2,9 +2,28 @@ package core
 
 import "time"
 
-var (
-	TimezoneKST, _ = time.LoadLocation("Asia/Seoul")
-	TimezoneJST, _ = time.LoadLocation("Asia/Tokyo")
-	TimezoneUTC, _ = time.LoadLocation("UTC")
-	TimezoneGMT, _ = time.LoadLocation("GMT")
+const (
+	DateTimeFormatAddTimeZone = "2006-01-02 15:04:05 MST"
 )
+
+var (
+	TimeZoneKST, _ = time.LoadLocation("Asia/Seoul")
+	TimeZoneJST, _ = time.LoadLocation("Asia/Tokyo")
+	TimeZoneUTC, _ = time.LoadLocation("UTC")
+)
+
+func ParseDatetimeInTimeZone(layout string, value string, TZ *time.Location) (time.Time, error) {
+	t, e := time.ParseInLocation(layout, value, TZ)
+	if e != nil {
+		return time.Time{}, e
+	}
+
+	switch TZ {
+	case TimeZoneKST, TimeZoneJST:
+		return t.Add(9 * time.Hour), nil
+	case TimeZoneUTC:
+		return t.UTC(), nil
+	}
+
+	return t.Local(), nil
+}
